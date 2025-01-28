e3d32dac8ea56634a384014fe0a6e3425bebfa2d
teixasz <joaopedroteixeirareis@gmail.com>
2025-01-28 11:34:14 -0300

    mudanças na conexão e interações com o sql

--- LojaProjeto/Connect.cs
+++ LojaProjeto/Connect.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +27,60 @@ namespace LojaProjeto
                 MessageBox.Show("Erro: " + ex.Message);
             }
 
+        }
+
+        //Função para executar comandos no banco de dados
+        int resultado;
+        public int ExecutarComandos(string sql)
+        {
+            try
+            {
+                //faz a conecção com o banco de dados
+                ConectarBD();
+                //prepara para a execução do comando
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                //executa o comando
+                cmd.ExecuteNonQuery();
+                resultado = 1;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro: " + e.Message);
+                resultado = 0;
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return resultado;
+        }
+
+        //funcao para exibir uma tabela de dados
+
+        public DataTable ExecutarTabela(string sql)
+        {
+            try
+            {
+                //faz a conecção com o banco de dados
+                ConectarBD();
+                //prepara para a execução do comando
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                //executa o comando
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erro: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
